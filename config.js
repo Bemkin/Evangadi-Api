b628836ce789a218d87ff66c8ad6d56a85fa6f7a
Bemnet <bemnetkibret4@gmail.com>
2024-12-14 23:25:16 +0300

    backend done

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-// config.js
+require('dotenv').config();
+
 const config = {
-    host: 'localhost',     // Your MySQL host
-    user: 'root',          // Your MySQL user
-    password: 'zekiwos1', // Your MySQL password
-    database: 'evangadi_forum' // Your database name
-  };
-  
-  module.exports = config;
-  
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+};
+
+module.exports = config;
